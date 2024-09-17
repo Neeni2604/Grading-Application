6b515bfbc7fff6255c90be517cbdf91a479f9309
Neeni2604 <nvasa7707@gmail.com>
2024-09-16 23:15:06 -0600

    Trying to pass autograder

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+class Assignment(models.Model):
+    field = models.Field
+
+class Submission(models.Model):
+    print()
