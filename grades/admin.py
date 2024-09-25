c7e6adf2188580eddfb9a31e6092b7a806861112
Neeni2604 <nvasa7707@gmail.com>
2024-09-25 15:29:20 -0600

    completed until phase 4

--- grades/admin.py
+++ grades/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Assignment
+from .models import Submission
 
 # Register your models here.
+admin.site.register(Assignment)
+admin.site.register(Submission)
--- grades/views.py
+++ grades/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html" , {
+        "assignments" : models.Assignment.objects.all(),
+    })
 
 def assignment(request, assignment_id):
-    return render(request, "assignment.html")
+    assignment = models.Assignment.objects.get(id=assignment_id)
+    return render(request, "assignment.html" , {
+        "assignment" : assignment,
+        "number_of_students" : models.Group.objects.get(name="Students").user_set.count(),
+        "number_of_submissions" : assignment.submission_set.count(),
+        "number_of_submissions_assigned_to_you" : models.User.objects.get(username="g").graded_set.filter(assignment = assignment).count(),
+    })
 
 def submissions(request, assignment_id):
     return render(request, "submissions.html")
