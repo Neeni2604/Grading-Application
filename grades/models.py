d799c41cf2ead44ff8e1bbe9a39cb8a1c4a73426
Neeni2604 <nvasa7707@gmail.com>
2024-09-27 12:33:48 -0600

    I guess I am done with HW 3

--- grades/models.py
+++ grades/models.py
@@ -6,14 +6,14 @@ class Assignment(models.Model):
     title = models.CharField(max_length=200 , null=False , blank=False)
     description = models.TextField(null=False , blank=False)
     deadline = models.DateTimeField(null=False , blank=False)
-    weight = models.IntegerField(null=False , blank=False , default=0)
-    points = models.IntegerField(null=False , blank=False , default=0)
+    weight = models.IntegerField(null=False , blank=False)
+    points = models.IntegerField(null=False , blank=False)
 
 class Submission(models.Model):
     assignment = models.ForeignKey(Assignment , on_delete=models.CASCADE)
     author = models.ForeignKey(User , on_delete=models.CASCADE)
-    grader = models.ForeignKey(User , on_delete=models.CASCADE , related_name='graded_set')
+    grader = models.ForeignKey(User , on_delete=models.CASCADE , related_name='graded_set', null=True, blank=True)
     file = models.FileField(null=False , blank=False)
-    score = models.FloatField(null=True , default=0)
+    score = models.FloatField(null=True, blank=True)
 
     
--- grades/views.py
+++ grades/views.py
@@ -1,5 +1,6 @@
 from . import models
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -9,27 +10,47 @@ def index(request):
 
 
 def assignment(request, assignment_id):
-    assignment = models.Assignment.objects.get(id=assignment_id)
-    return render(request, "assignment.html" , {
+    try:
+        assignment = models.Assignment.objects.get(id=assignment_id)
+        return render(request, "assignment.html" , {
         "assignment" : assignment,
         "number_of_students" : models.Group.objects.get(name="Students").user_set.count(),
         "number_of_submissions" : assignment.submission_set.count(),
         "number_of_submissions_assigned_to_you" : models.User.objects.get(username="g").graded_set.filter(assignment = assignment).count(),
-    })
+        })
+    except models.Assignment.DoesNotExist:
+        raise Http404("Hello")
 
 
 def submissions(request, assignment_id):
-    assignment = models.Assignment.objects.get(id=assignment_id)
-    submissions = assignment.submission_set.order_by("author")
+    try:
+        assignment = models.Assignment.objects.get(id=assignment_id)
+        garry_grader = models.User.objects.get(username="g")
+        submissions = assignment.submission_set.filter(grader=garry_grader).order_by("author")
 
-    return render(request, "submissions.html" , {
+        return render(request, "submissions.html" , {
         "assignment" : assignment,
         "submissions" : submissions,
-    })
+        })
+    except models.Assignment.DoesNotExist:
+        raise Http404("Hello")
+    
 
 
 def profile(request):
-    return render(request, "profile.html")
+    assignments = models.Assignment.objects.all()
+    garry_grader = models.User.objects.get(username="g")
+
+    assignment_graded_column = []
+
+    for assignment in assignments:
+        submissions_assigned_to_you = assignment.submission_set.filter(grader=garry_grader).count()
+        submissions_you_graded = assignment.submission_set.filter(grader=garry_grader, score__isnull=False).count()
+        assignment_graded_column.append({"assignment":assignment, "submissions_you_graded":submissions_you_graded ,"submissions_assigned_to_you": submissions_assigned_to_you})
+
+    return render(request, "profile.html", {
+        "assignment_graded_column": assignment_graded_column,
+    })
 
 
 def login_form(request):
