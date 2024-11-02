34f6945af0431ff1c4ae2d9a08cd525602750ac6
Neeni2604 <nvasa7707@gmail.com>
2024-11-02 13:23:18 -0600

    phase 1

--- cs3550/urls.py
+++ cs3550/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path("<int:assignment_id>/", views.assignment),
     path("<int:assignment_id>/submissions", views.submissions),
     path("profile/", views.profile),
-    path("profile/login/", views.login_form),
+    path("profile/login", views.login_form),
     path('uploads/<str:filename>', views.show_upload),
     path("profile/logout/", views.logout_form),
+    path("profile/login/", views.login_form),
 ]
